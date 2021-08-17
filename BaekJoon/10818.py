90d898cb9d2c4509034b2aeff4ffac4fc4be4d4b
hanmin <hanmin.more@gmail.com>
2021-08-17 22:40:06 +0900

    Create 10818.py

---
+++ BaekJoon/10818.py
@@ -0,0 +1,12 @@
+cnt = int(input())
+num = list(map(int, input().split()))
+max = num[0]
+min = num[0]
+
+for i in num[1:]:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+
+print(min,max)
