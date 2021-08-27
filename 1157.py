03476f1a7b1a1a221d71ee0ce4738f57e6e40a01
hanmin <hanmin.more@gmail.com>
2021-08-27 23:48:15 +0900

    Update 1157.py

--- 1157.py
+++ 1157.py
@@ -14,10 +14,10 @@ distinct_s = list(set(s))  # 입력받은 문자열에서 중복값을 제거
 cnt_list = []
 for x in distinct_s :
     cnt = s.count(x)
-    cnt_list.append(cnt)  # count 숫자를 리스트에 append
+    cnt_list.append(cnt)  
 
-if cnt_list.count(max(cnt_list)) > 1 :  # count 숫자 최대값이 중복되면
+if cnt_list.count(max(cnt_list)) > 1 :
     print('?')
 else :
-    max_index = cnt_list.index(max(cnt_list))  # count 숫자 최대값 인덱스(위치)
+    max_index = cnt_list.index(max(cnt_list)) 
     print(distinct_s[max_index])
